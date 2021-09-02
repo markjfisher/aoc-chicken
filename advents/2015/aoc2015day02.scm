d26b66eb41a363ad3705a5bbe19b15b08e0cd96a
Mark Fisher <mark.j.fisher@gmail.com>
2021-09-02 01:11:19 +0100

    Implement 2015 day 2 part 1

--- advents/2015/aoc2015day02.scm
+++ advents/2015/aoc2015day02.scm
@@ -7,15 +7,29 @@
 
   (import scheme format
           (chicken base)
-          (streams utils)
-          srfi-1 srfi-41
+          (streams utils) (streams derived)
+          srfi-1 srfi-13 srfi-41
+          matchable
           aoc-files)
 
   (define (aoc2015day02::part1)
-    0)
+    (stream-fold (lambda (s x) (+ s x))
+                 0
+                 (surface-area-stream (aoc-resource-stream-lines 2015 2))))
 
   (define (aoc2015day02::part2)
-    (let ([xxx 1] [yyy 2] [zzz 4])
-      xxx zzz))
+    1)
+
+  ;; from a stream of values like 2x3x4, extract the dimensions to l,w,h and then
+  ;; create a stream of values that represent all the surface areas ADDED for a given l x w x h
+  ;; which are 2*l*w, 2*w*h, 2*h*l, (min l,w,h) * (2nd-min l,w,h)
+  (define (surface-area-stream stream)
+    (stream-map (lambda (lwh)
+                  (if (string-null? lwh)
+                      0
+                      (match-let* ([(l w h) (map string->number (string-split lwh "x"))]
+                                   [(a b) (take (sort (list l w h) <) 2)])
+                        (+ (* 2 l w) (* 2 w h) (* 2 h l) (* a b)))))
+                stream))
 
   )
--- advents/aoc-files.scm
+++ advents/aoc-files.scm
@@ -15,13 +15,36 @@
   ;; ----------------------------------------------------
   ;;; ## aoc-resource-stream
   ;; ----------------------------------------------------
-  ;;; return a stream for given YEAR DAY values from resource dir
+  ;;; return a stream of chars for given YEAR DAY resource file
   ;;; ```scheme
   ;;; ; get the resource for 2015 day 1
   ;;; (let ((fs (aoc-resource-stream 2015 1)))
   ;;;   ...)
+  ;;; <lazy stream = (#\a #\b #\c ... from resource file)>
   ;;; ```
   (define (aoc-resource-stream year day)
     (file->stream (sprintf "~a/resources/day~a.txt" year day)))
+
+
+  ;;; return a stream of lines for given YEAR DAY resource file
+  (define (aoc-resource-stream-lines year day)
+    (stream-map (lambda (s)
+                  (list->string (stream->list s)))
+                (lines (aoc-resource-stream year day))))
+
+  (define (breakon a)
+    (stream-lambda (x xss)
+                   (if (equal? a x)
+                       (stream-append (stream (stream)) xss)
+                       (stream-append
+                        (stream (stream-append
+                                 (stream x) (stream-car xss)))
+                        (stream-cdr xss)))))
+
+  (define-stream (lines strm)
+    (stream-fold-right
+     (breakon #\newline)
+     (stream (stream))
+     strm))
 
   ) ;; end of aoc-files module
