0ae0b08fe560d92af7d994161faeeac6ba7e002e
Mark Fisher <mark.j.fisher@gmail.com>
2021-09-06 11:14:15 +0100

    Much faster md5 sums without gochans

--- advents/2015/aoc2015day04.scm
+++ advents/2015/aoc2015day04.scm
@@ -10,36 +10,52 @@
           (only (chicken random) pseudo-random-integer)
           (streams utils) (streams derived)
           srfi-1 srfi-13 srfi-18 srfi-41 srfi-113 srfi-128
-          matchable
-          gochan simple-md5 miscmacros
+          matchable gochan simple-md5 miscmacros md5 message-digest-byte-vector simple-loops
           )
 
   (define secret "bgvyzdsv")
+  (define prim (md5-primitive))
 
   (define (aoc2015day04::part1)
-    (find-hash "00000" 254570))
+    ;; (find-hash "00000" 254570)
+    (find-hash "00000" 0)
+    ;; (direct-hashing "00000" 5)
+    )
 
   (define (aoc2015day04::part2)
-    (find-hash "000000" 1038730))
+    ;; (find-hash "000000" 1038730)
+    ;; (find-hash "000000" 0)
+    (direct-hashing "000000" 6)
+    )
+
+  ;; this is SO much faster than the gochan version!
+  (define (direct-hashing match length)
+    (let ([n 0]
+          [prim (md5-primitive)])
+      (do-until (substring=? (message-digest-string prim (conc secret n)) match 0 0 length)
+                (set! n (add1 n)))
+      n)
+    )
 
   (define (info . args) (apply print (cons (current-thread) (cons " " args))))
 
   (define (hash-tester match listen-chan reply-chan found-chan)
-    (let loop ()
-      (gochan-select
-       ((listen-chan -> num)
-        (if (substring=? (string->md5sum (conc secret num)) match 0 0 (string-length match))
-            (gochan-send found-chan num))
-        (gochan-send reply-chan num)
-        (loop)))))
+    (let ([strlen (string-length match)])
+      (let loop ()
+        (gochan-select
+         ((listen-chan -> num)
+          (if (substring=? (message-digest-string prim (conc secret num)) match 0 0 strlen)
+              (gochan-send found-chan num))
+          (gochan-send reply-chan num)
+          (loop))))))
 
   (define (find-hash match start-guess)
-    (let ([worker-chan (gochan 200)]
-          [result-chan (gochan 200)]
-          [found-chan (gochan 0)]
-          [block-size 200]
-          [result 0])
-      (repeat block-size (go (hash-tester match worker-chan result-chan found-chan)))
+    (let* ([block-size 50000]
+           [worker-chan (gochan block-size)]
+           [result-chan (gochan block-size)]
+           [found-chan (gochan 0)]
+           [result 0])
+      (repeat 8 (go (hash-tester match worker-chan result-chan found-chan)))
 
       (let loop ([num start-guess]
                  [running #t])
